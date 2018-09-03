1e6125ca22de8ebc0e3da92e79a06e98011f11b7
Thomas <thomasg@fligno.com.ph>
2018-09-03 10:15:44 +0800

    Remove old functions on App.js

--- src/App.js
+++ src/App.js
@@ -5,37 +5,6 @@ import Counters from './components/counters'
 import './App.css'
 
 class App extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 6 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ]
-  }
-
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters]
-    const index = counters.indexOf(counter)
-
-    counters[index].value++
-
-    this.setState({ counters })
-  }
-
-  handleDelete = (counterId) => {
-    const counters = this.state.counters.filter(counter => counter.id !== counterId)
-    this.setState({ counters })
-  }
-
-  handleReset = () => {
-    const counters = this.state.counters.map(counter => {
-      counter.value = 0
-      return counter
-    })
-    this.setState({ counters })
-  }
-
   render() {
     return (
       <React.Fragment>
